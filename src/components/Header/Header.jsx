68082dee0788171387700c201d42c25ddadebf18
Josefine Forsman <josefineforsman1@gmail.com>
2023-10-03 15:10:37 +0200

    first base on searchbar functionality

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,15 +1,14 @@
 import "./Header.scss";
-import Navbar from '../Navbar/Navbar'
-import Searchbar from '../SearchBar/SearchBar'
+import Navbar from "../Navbar/Navbar";
+import Searchbar from "../SearchBar/SearchBar";
 
-function Header() {
+function Header({ allMovies }) {
   return (
     <div>
-      <Navbar/>
-      <Searchbar/>
+      <Navbar />
+      <Searchbar allMovies={allMovies} />
     </div>
-  
-  )
+  );
 }
 
 export default Header;
--- src/components/SearchBar/SearchBar.jsx
+++ src/components/SearchBar/SearchBar.jsx
@@ -1,11 +1,38 @@
 import "./SearchBar.scss";
+import { useState } from "react";
 
-function SearchBar() {
+function SearchBar({ allMovies }) {
+  const [inputValue, setInputValue] = useState("");
+  const [errorMessage, setErrorMessage] = useState("");
+  console.log(allMovies);
+
+  function handleKeyPress(e) {
+    if (e.key === "Enter") {
+      const movieFound = allMovies.find(
+        (movie) => movie.title.toLowerCase() === inputValue.toLowerCase()
+      );
+
+      if (!movieFound) {
+        setErrorMessage("Movie not found. Please try another search.");
+      } else {
+        setErrorMessage("Movie is found");
+        console.log("Enter is working as a click");
+      }
+    }
+  }
   return (
     <div className="SearchBar">
-      <input className="SearchBar__input" type="text" placeholder="Search movies here..." />
+      <input
+        className="SearchBar__input"
+        value={inputValue}
+        onChange={(e) => setInputValue(e.target.value)}
+        onKeyDown={handleKeyPress}
+        type="text"
+        placeholder="Search movies here..."
+      />
+      {errorMessage && <div className="error-message">{errorMessage}</div>}
     </div>
-  )
+  );
 }
 
 export default SearchBar;
