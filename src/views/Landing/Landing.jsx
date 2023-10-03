fca25e856cfc64572afc95248b0a8371abeaaa4d
joakimfr <fritzjoakim@hotmail.com>
2023-10-03 14:16:19 +0200

    fetching data from json is working

--- src/views/Landing/Landing.jsx
+++ src/views/Landing/Landing.jsx
@@ -1,9 +1,30 @@
 import "./Landing.sass";
+import { useState, useEffect } from "react";
 import DisplayCarousel from "../../components/DisplayCarousel/DisplayCarousel";
 import Header from "../../components/Header/Header";
+import moviesData from '../../../movies.json'
 
 function Landing() {
-  return (
+
+const [allMovies, setAllMovies] = useState([]);
+
+console.log(allMovies)
+
+useEffect(() => {
+  function getMovies() {
+    try {
+
+      const data = moviesData;
+      setAllMovies(data);
+
+    } catch (error) {
+      console.error('Couldnt find any movies', error);
+    }
+  }
+  getMovies();
+}, []);
+
+ return (
     <div>
       <Header/>
       <DisplayCarousel />
