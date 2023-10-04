4755cd38a967d03124ce3ba0ab44be5e164b074f
Josefine Forsman <josefineforsman1@gmail.com>
2023-10-04 11:34:05 +0200

    added footer in bookmarks + category view

--- src/views/Bookmarks/Bookmarks.jsx
+++ src/views/Bookmarks/Bookmarks.jsx
@@ -1,16 +1,16 @@
 import "./Bookmarks.sass";
 import SingleMovie from "../../components/SingleMovie/SingleMovie";
 import Header from "../../components/Header/Header";
-
+import Footer from "../../components/Footer/Footer";
 
 function Bookmarks() {
-
   return (
     <div>
-      <Header/>
+      <Header />
       <SingleMovie />
+      <Footer />
     </div>
-  )
+  );
 }
 
 export default Bookmarks;
