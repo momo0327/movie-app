484e9cc7c1e08ec1ba2a338db2a34d4a4637ab21
momo0327 <mohamedsafe0427@gmail.com>
2023-10-04 13:50:30 +0200

    displaying all trending movies in singlemovies component and then filter out to displaycarousel then to landing page

--- src/views/Landing/Landing.jsx
+++ src/views/Landing/Landing.jsx
@@ -9,15 +9,21 @@ import SingleMovie from "../../components/SingleMovie/SingleMovie";
 function Landing() {
   const [allMovies, setAllMovies] = useState([]);
 
-  const content = allMovies?.map((trend)=> {   // mappar ut allMovies för att få ut all trending movies
-    if(trend.isTrending){   // om is trending finns så ska den returna titeln på trending filmen
-      console.log(trend.title);
-      return <DisplayCarousel  trendTitle ={trend.title}   />
-    //  return <div> {trend.title}   </div>
-    }
+  // const content = allMovies?.map((trend)=> {   // mappar ut allMovies för att få ut all trending movies
+  //   if(trend.isTrending){   // om is trending finns så ska den returna titeln på trending filmen
+  //     console.log(trend);
+  //     return <DisplayCarousel  trendMovies={trend}   />
     
-  })
-  
+  //   }
+    
+  // })
+
+function getTrendingMovies(movies){
+  return movies.filter((movie) => movie.isTrending)
+}
+
+const trendingMovies = getTrendingMovies(allMovies)
+
   useEffect(() => {
     function getMovies() {
       try {
@@ -34,7 +40,7 @@ function Landing() {
     <div className="landing">
       
       <Header allMovies={allMovies} />
-      <DisplayCarousel />
+      <DisplayCarousel genreMovies={trendingMovies} />
       <Footer />
     </div>
   );
