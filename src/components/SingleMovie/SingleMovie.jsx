86ac8948a01fc656d9d63b0f3c718d74e929864e
joakimfr <fritzjoakim@hotmail.com>
2023-10-06 13:39:13 +0200

    handle error image
    error image is now showing in all landings.
    
    Need to add css to filmview and bookmarks

--- src/components/SingleMovie/SingleMovie.jsx
+++ src/components/SingleMovie/SingleMovie.jsx
@@ -3,20 +3,26 @@ import { useState } from "react";
 import { useNavigate } from "react-router";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faBookmark } from "@fortawesome/free-solid-svg-icons";
+import missingImage from '../../assets/missing-image.svg';
 
 function SingleMovie({ title, year, thumbnail, genre, actors, synopsis }) {
   const [isFavorite, setIsFavorite] = useState(false);
+  const [imageError, setImageError] = useState(false)
   const navigate = useNavigate();
 
+  const handleImageError = () => {
+    setImageError(true);
+  }
+
   const handleViewFilm = () => {
-    const movieData = { title, year, thumbnail, genre, actors, synopsis };
+    const movieData = { title, year, thumbnail: imageError ? missingImage : thumbnail, genre, actors, synopsis };
     localStorage.setItem("selectedMovie", JSON.stringify(movieData));
     console.log(movieData);
     navigate("/movie-app/film-view");
   };
 
   const handleFavoriteWord = () => {
-    const storedMovieData = { title, thumbnail };
+    const storedMovieData = { title, thumbnail: imageError ? missingImage : thumbnail };
     const storedMovies =
       JSON.parse(localStorage.getItem("favoriteMovies")) || [];
     storedMovies.push(storedMovieData);
@@ -29,12 +35,25 @@ function SingleMovie({ title, year, thumbnail, genre, actors, synopsis }) {
     <div>
       <article className="movie-card">
         <div className="movie-card__image-container">
+
+        {imageError ? (
+        <div className="placeholder">
           <img
-            className="movie-card__image"
-            src={thumbnail}
-            alt="movie-img"
+            className="placeholder__image"
+            src={missingImage}
+            alt=""
             onClick={handleViewFilm}
           />
+        </div>
+      ) : (
+        <img
+          className="movie-card__image"
+          src={thumbnail}
+          alt="movie-img"
+          onClick={handleViewFilm}
+          onError={handleImageError}
+        />
+      )}
           <div className="movie-card__info">
             <h4 className="movie-card__title">{title}</h4>
             <FontAwesomeIcon
