f23bf376b708ca624f45b947f42401b0986e4fe9
momo0327 <mohamedsafe0427@gmail.com>
2023-10-09 14:03:42 +0200

    rebased and removed unused code

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -1,11 +1,11 @@
 import { describe, it, expect } from 'vitest';
 import { render, screen } from '@testing-library/react';
-import App from './App';
+
 import { BrowserRouter } from "react-router-dom";
-import Categories from './views/Categories/Categories';
+
 import Landing from './views/Landing/Landing';
 import userEvent from "@testing-library/user-event";
-import { log } from 'console';
+
 
 
 
