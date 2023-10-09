82cf56fc3413e7cc06effcec136333b3e732eecf
Josefine Forsman <josefineforsman1@gmail.com>
2023-10-09 14:16:19 +0200

    added funtionality to bookmark in filmView

--- src/components/SingleMovie/SingleMovie.jsx
+++ src/components/SingleMovie/SingleMovie.jsx
@@ -3,33 +3,43 @@ import { useState } from "react";
 import { useNavigate } from "react-router";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faBookmark } from "@fortawesome/free-solid-svg-icons";
-import missingImage from '../../assets/noimage.jpeg'
+import missingImage from "../../assets/noimage.jpeg";
 
 function SingleMovie({ title, year, thumbnail, genre, actors, synopsis }) {
   const [isFavorite, setIsFavorite] = useState(false);
-  const [imageError, setImageError] = useState(false)
+  const [imageError, setImageError] = useState(false);
   const navigate = useNavigate();
 
   const handleImageError = () => {
     setImageError(true);
-  }
+  };
 
   const handleViewFilm = () => {
-    const movieData = { title, year, thumbnail: imageError ? missingImage : thumbnail, genre, actors, synopsis };
+    const movieData = {
+      title,
+      year,
+      thumbnail: imageError ? missingImage : thumbnail,
+      genre,
+      actors,
+      synopsis,
+    };
     localStorage.setItem("selectedMovie", JSON.stringify(movieData));
     console.log(movieData);
     navigate("/movie-app/film-view");
   };
 
   const handleFavoriteMovie = () => {
-    const storedMovieData = { title, thumbnail: imageError ? missingImage : thumbnail }; //filmen man klickat på
+    const storedMovieData = {
+      title,
+      thumbnail: imageError ? missingImage : thumbnail,
+    }; //filmen man klickat på
     const storedMovies =
       JSON.parse(localStorage.getItem("favoriteMovies")) || [];
     let movieAlreadyInList = false;
 
-    console.log(storedMovies) //loggar listan med filmer
+    console.log(storedMovies); //loggar listan med filmer
 
-    storedMovies.forEach(movie => {
+    storedMovies.forEach((movie) => {
       if (movie.title === storedMovieData.title) {
         console.log("Filmen finns redan i listan");
         movieAlreadyInList = true;
@@ -37,10 +47,10 @@ function SingleMovie({ title, year, thumbnail, genre, actors, synopsis }) {
       }
     });
 
-    if(movieAlreadyInList==false){
+    if (movieAlreadyInList == false) {
       storedMovies.push(storedMovieData); //lägger till filmen i listan
-        localStorage.setItem("favoriteMovies", JSON.stringify(storedMovies)); //sparar den uppdaterade listan i localstorage
-        setIsFavorite(true);
+      localStorage.setItem("favoriteMovies", JSON.stringify(storedMovies)); //sparar den uppdaterade listan i localstorage
+      setIsFavorite(true);
     }
   };
 
@@ -48,26 +58,22 @@ function SingleMovie({ title, year, thumbnail, genre, actors, synopsis }) {
     <div>
       <article className="movie-card" role="movie-card">
         <div className="movie-card__image-container">
-
-        {imageError ? (
-        
-          <img
-            className="movie-card__image"
-            src={missingImage}
-            onClick={handleViewFilm}
-            alt=""
-            
-          />
-       
-      ) : (
-        <img
-          className="movie-card__image"
-          src={thumbnail}
-          alt="movie-img"
-          onClick={handleViewFilm}
-          onError={handleImageError}
-        />
-      )}
+          {imageError ? (
+            <img
+              className="movie-card__image"
+              src={missingImage}
+              onClick={handleViewFilm}
+              alt=""
+            />
+          ) : (
+            <img
+              className="movie-card__image"
+              src={thumbnail}
+              alt="movie-img"
+              onClick={handleViewFilm}
+              onError={handleImageError}
+            />
+          )}
           <div className="movie-card__info">
             <h4 className="movie-card__title">{title}</h4>
             <FontAwesomeIcon
