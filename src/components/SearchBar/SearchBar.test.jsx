56da2ac76e2573aefc4c03aa84ee646ba29da023
erikkjell-1 <erkan26@live.se>
2023-10-09 13:59:33 +0200

    bugfix on searchbar test

--- src/components/SearchBar/SearchBar.test.jsx
+++ src/components/SearchBar/SearchBar.test.jsx
@@ -1,34 +1,27 @@
-import { it, expect } from 'vitest';
+import { it, expect, describe } from 'vitest';
 import { render, screen } from '@testing-library/react';
 import SearchBar from './SearchBar';
 import userEvent from "@testing-library/user-event";
 import movies from '../../../movies.json'
 
-
+describe(SearchBar, () => {
   it('should accept the user typing', async () => { //testar att användaren kan skriva
     render(<SearchBar allMovies={movies}/>);
-
     await userEvent.type(screen.getByRole('textbox'), "titanic");
     expect(screen.getByRole('textbox')).toHaveValue("titanic")
-
   });
-
   it('should tell the user when a movie is found', async() => { //testar meddelande att filmen hittas
     //denna behöver nog ändras till att filmens titel visas på skärmen senare!
-
     render(<SearchBar allMovies={movies}/>);
-
     await userEvent.type(screen.getByRole('textbox'), "the shawshank redemption{Enter}");
     //enter
-    expect(await screen.findByText("the shawshank redemption")).toBeInTheDocument();
-
+    expect(await screen.findByText("the shawshank redemption", {exact: false})).toBeInTheDocument();
   })
 
   it('should tell the user when a movie is not found', async () =>{ //testar felmeddelande
-
     render(<SearchBar allMovies={movies}/>);
-
     await userEvent.type(screen.getByRole('textbox'), "fake movie haha!!{Enter}")
     //enter
-    expect(await screen.findByText('Movie not found. Please try another search.')).toBeInTheDocument();
+    expect(await screen.findByText('no movies found', {exact: false})).toBeInTheDocument();
   })
+});
