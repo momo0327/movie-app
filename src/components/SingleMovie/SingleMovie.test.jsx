6e3b863e48bc826df377b435825efb495de28550
joakimfr <fritzjoakim@hotmail.com>
2023-10-10 14:09:00 +0200

    removed linting errors

--- src/components/SingleMovie/SingleMovie.test.jsx
+++ src/components/SingleMovie/SingleMovie.test.jsx
@@ -1,7 +1,7 @@
 import SingleMovie from "./SingleMovie";
 import { render, screen } from '@testing-library/react';
-import userEvent from "@testing-library/user-event";
 import { BrowserRouter } from 'react-router-dom';
+import { it, expect } from 'vitest';
 
 
 it('should render singleMovie component with props', () => {
